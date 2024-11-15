388a5c3aecfd5e6d17ccd29c24a99210a735b404
PinkWink <pinkwink.korea@gmail.com>
2024-11-15 16:43:09 +0900

    Use plain image instead of compressed

--- robot.py
+++ robot.py
@@ -5,7 +5,7 @@ import rospy
 import cv2
 import numpy as np
 
-from sensor_msgs.msg import CompressedImage
+from sensor_msgs.msg import CompressedImage, Image
 from cv_bridge import CvBridge
 from geometry_msgs.msg import Twist
 
@@ -13,12 +13,13 @@ from drive import Drive
 
 
 def warping(image):
+    sz = 120
     source = np.float32([[147, 104], [0, 343], [487, 113], [636, 355]])
-    destination = np.float32([[0, 0], [0, 480], [480, 0], [480, 480]])
+    destination = np.float32([[0, 0], [0, sz], [sz, 0], [sz, sz]])
     
     M = cv2.getPerspectiveTransform(source, destination)
     
-    warp_image = cv2.warpPerspective(image, M, (480, 480), flags=cv2.INTER_LINEAR)
+    warp_image = cv2.warpPerspective(image, M, (120, 120), flags=cv2.INTER_LINEAR)
 
     return warp_image
 
@@ -30,12 +31,12 @@ class RobotControlNode():
         self.bridge = CvBridge()
         rospy.init_node('robot_control_node', anonymous=False)
         #rospy.Subscriber('/main_camera/image_raw/compressed', CompressedImage, self.camera_callback)
-        rospy.Subscriber('/main_camera/image_raw', CompressedImage, self.camera_callback)
+        rospy.Subscriber('/main_camera/image_raw', Image, self.camera_callback)
         self.pub = rospy.Publisher("/cmd_vel", Twist, queue_size=1)
 
     
     def camera_callback(self, data):
-        image = self.bridge.compressed_imgmsg_to_cv2(data, desired_encoding="bgr8")
+        image = self.bridge.imgmsg_to_cv2(data, desired_encoding="bgr8")
         image = warping(image)
         self.drive.on_camera(image)
         cv2.waitKey(1)
